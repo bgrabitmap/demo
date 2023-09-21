2ea700bb44a1860a207ebd5011379d6f086347f5
Johann ELSASS <circular@operamail.com>
2023-09-21 09:35:22 +0200

    render only moving parts, stop when loop complete

--- hypocycloid/umain.pas
+++ hypocycloid/umain.pas
@@ -11,6 +11,9 @@ unit umain;
   - DPI size
   - refactor
   - updown to choose nb of cusps
+ v0.3
+  - render only moving parts
+  - stop when loop complete
 }
 
 
@@ -21,7 +24,7 @@ interface
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Spin,
   BGRAVirtualScreen, BGRABitmap, BGRABitmapTypes, BCTypes, BCTrackbarUpdown,
-  Math;
+  Math, BGRAClasses;
 
 type
 
@@ -35,12 +38,16 @@ type
     procedure BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
     procedure BGRAVirtualScreen1Resize(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
   private
     toReset: boolean;
     nR, r, px, phi: Double;
     center: TPointF;
+    buffer: TBGRABitmap;
+    cursorBox: TRectF;
     procedure ResetShape;
+    function UpdateShape(Bitmap: TBGRABitmap): TRectF;
   public
 
   end;
@@ -64,12 +71,96 @@ begin
   result.y := (nR - r) * sin(phi) - r * sin((nR - r) / r * phi);
 end;
 
+function BoundingBox(ARectF: TRectF; Margin: Integer = 1): TRect;
+begin
+  if ARectF.IsEmpty then
+    result := EmptyRect
+  else
+  with ARectF do
+    result := BGRAClasses.Rect(floor(Left) - Margin, floor(Top) - Margin,
+                               ceil(Right) + Margin, ceil(Bottom) + Margin);
+end;
+
 { TfrmMain }
 
-procedure TfrmMain.BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
+function TfrmMain.UpdateShape(Bitmap: TBGRABitmap): TRectF;
+const phiStep = 0.01;
 var
-  t: Double = 0;
-  ptCenter, pt: TPointF;
+  ptCenter, pt, ptBefore: TPointF;
+  r1, r2: Double;
+  lineBox, cursorBoxBefore: TRectF;
+  phiMod: Double;
+begin
+  // restore buffer under cursor
+  cursorBoxBefore := cursorBox;
+  if Assigned(buffer) and not cursorBox.IsEmpty then
+  begin
+    Bitmap.ClipRect := BoundingBox(cursorBox);
+    Bitmap.PutImage(0, 0, buffer, dmSet);
+    Bitmap.NoClip;
+    cursorBox := EmptyRectF;
+  end;
+
+  if phi >= 2*Pi-phiStep/2 then
+  begin
+    // if loop is complete, then stops
+    if VectLen(HypocycloidAt(phi, nR, r) - HypocycloidAt(0, nR, r)) < 2*px then
+       exit(cursorBoxBefore);
+  end;
+
+  with Bitmap.Canvas2D do
+  begin
+    lineWidth := 1*px;
+
+    // draw the curve
+    ptBefore := HypocycloidAt(phi, nR, r) + center;
+    phi += phiStep;
+    pt := HypocycloidAt(phi, nR, r) + center;
+    beginPath();
+    moveTo(ptBefore);
+    lineTo(pt);
+    strokeStyle(clRed);
+    save();
+    lineWidth := 3*px;
+    stroke();
+    lineBox := RectF(ptBefore.x - lineWidth/2, ptBefore.y - lineWidth/2,
+                     ptBefore.x + lineWidth/2, ptBefore.y + lineWidth/2)
+               .Union(RectF(pt.x - lineWidth/2, pt.y - lineWidth/2,
+                            pt.x + lineWidth/2, pt.y + lineWidth/2));
+    restore();
+
+    // save buffer without cursor
+    if Assigned(buffer) and not lineBox.IsEmpty then
+    begin
+      buffer.ClipRect := BoundingBox(lineBox);
+      buffer.PutImage(0, 0, Bitmap, dmSet);
+      buffer.NoClip;
+    end;
+
+    // draw the cursor
+    ptCenter := HypocycloidCenterAt(phi, nR, r) + center;
+    strokeStyle(BGRA(51, 51, 51));
+    beginPath();
+    r1 := r + 0.5*px; // taking pen width into account
+    circle(ptCenter.x, ptCenter.y, r);
+    moveTo(ptCenter);
+    lineTo(pt);
+    stroke();
+
+    beginPath();
+    r2 := 3*px;
+    circle(pt.x, pt.y, r2);
+    fill();
+
+    cursorBox := RectF(ptCenter.x - r1, ptCenter.y - r1, ptCenter.x + r1, ptCenter.y + r1)
+                 .Union(RectF(pt.x - r2, pt.y - r2, pt.x + r2, pt.y + r2));
+  end;
+
+  result := lineBox.Union(cursorBoxBefore).Union(cursorBox);
+end;
+
+
+procedure TfrmMain.BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
 begin
   if toReset then
   begin
@@ -85,53 +176,24 @@ begin
 
     phi := 0;
     toReset := false;
-  end;
-
-  with Bitmap.Canvas2D do
-  begin
-    lineWidth := 1*px;
-
-    // bounding circle
-    strokeStyle(clBlue);
-    beginPath();
-    circle(center.x, center.y, nR);
-    stroke();
-
-    // draw the cursor
-    ptCenter := HypocycloidCenterAt(phi, nR, r) + center;
-    pt := HypocycloidAt(phi, nR, r) + center;
-
-    strokeStyle(BGRA(51, 51, 51));
-    beginPath();
-    circle(ptCenter.x, ptCenter.y, r);
-    moveTo(ptCenter);
-    lineTo(pt);
-    stroke();
-
-    beginPath();
-    circle(pt.x, pt.y, 3*px);
-    fill();
-
-    // draw the curve
-    beginPath();
-    while (t <= phi) do
+    FreeAndNil(buffer);
+    buffer := TBGRABitmap.Create(Bitmap);
+    with buffer.Canvas2D do
     begin
-      pt := HypocycloidAt(t, nR, r) + center;
-      lineTo(pt);
-      t+=0.01;
+      lineWidth := 1*px;
+
+      // bounding circle
+      strokeStyle(clBlue);
+      beginPath();
+      circle(center.x, center.y, nR);
+      stroke();
     end;
-    strokeStyle(clRed);
-    save();
-    lineWidth := 3*px;
-    stroke();
-    restore();
+    cursorBox := EmptyRectF;
   end;
 
-  // next position
-  {if (phi >= 2 * PI) then
-    phi := 0
-  else}
-    phi += 0.01;
+  if Assigned(buffer) then
+    Bitmap.PutImage(0, 0, buffer, dmSet);
+  UpdateShape(Bitmap);
 end;
 
 procedure TfrmMain.BCTrackbarUpdownNCuspedChange(Sender: TObject;
@@ -147,17 +209,32 @@ end;
 
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
+  buffer := nil;
+  cursorBox := EmptyRectF;
   ResetShape;
 end;
 
-procedure TfrmMain.Timer1Timer(Sender: TObject);
+procedure TfrmMain.FormDestroy(Sender: TObject);
 begin
-  BGRAvirtualScreen1.DiscardBitmap;
+  FreeAndNil(buffer);
+end;
+
+procedure TfrmMain.Timer1Timer(Sender: TObject);
+var
+  modifyBox: TRectF;
+begin
+  if Assigned(BGRAVirtualScreen1.Bitmap) and not toReset then
+  begin
+    modifyBox := UpdateShape(BGRAVirtualScreen1.Bitmap);
+    if not IsEmptyRectF(modifyBox) then
+       BGRAVirtualScreen1.InvalidateBitmap(BoundingBox(modifyBox));
+  end;
 end;
 
 procedure TfrmMain.ResetShape;
 begin
   toReset := true;
+  BGRAVirtualScreen1.DiscardBitmap;
 end;
 
 end.
